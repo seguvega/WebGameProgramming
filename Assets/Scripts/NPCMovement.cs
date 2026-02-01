84356caacbcf3d5af02bbd6c44981472edd8ad6c
Sebastián Velasco <seguvega@hotmail.com>
2026-02-01 00:39:05 -0500

    Nav- Nav agent- waypoints

---
+++ Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NPCMovement : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private GameObject[] Waypoints;
+    private Vector3 Destination;
+    private int index;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        Waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+        if(Waypoints.Length <= 0) {return;}
+        index = Random.Range(0, Waypoints.Length);
+        agent.destination = Destination = Waypoints[index].transform.position;
+    }
+
+    void Update()
+    {
+        if(Waypoints.Length <= 0) {return;}
+        float distance = Vector3.Distance(transform.position, Destination);
+        //Debug.Log("Distance to destination: " + distance);
+        if(distance < 2.0f)
+        {
+            index = Random.Range(0, Waypoints.Length);
+            agent.destination = Destination = Waypoints[index].transform.position;
+
+        }
+    }
+}
